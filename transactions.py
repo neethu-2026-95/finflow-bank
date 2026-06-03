7c91d83e0a966ad92cab517eedbeef8493f39269
sajeeshs <sajie.kl8@gmail.com>
2026-06-03 16:06:02 +0530

    day2: added all 5 banking lists

--- transactions.py
+++ transactions.py
@@ -4,6 +4,12 @@ from datetime import datetime, timedelta
 
 branches = ["Chennai", "Mumbai", "Bangalore", "Delhi", "Hyderabad"]
 statuses = ["success", "success", "success", "failed", "pending"]
-
+account_types=["savings","current","salary","fd","rd","nri","joint","student","senior","business"]
+txn_types=["debit","credit"]
+countries=["India","UAE","USA"]
+               
 print(branches)
 print(statuses)
+print(account_types)
+print(txn_types)
+print(countries)
