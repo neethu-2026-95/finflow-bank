71ae475c737dc20cec85602317069675cf6af181
sajeeshs <sajie.kl8@gmail.com>
2026-06-03 15:41:06 +0530

    day1: variables and lists finflow bank

---
+++ transactions.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import random
+from datetime import datetime, timedelta
+
+branches = ["Chennai", "Mumbai", "Bangalore", "Delhi", "Hyderabad"]
+statuses = ["success", "success", "success", "failed", "pending"]
+
+print(branches)
+print(statuses)
