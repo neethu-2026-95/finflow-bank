652cad422dbe68ef9a44ca1f6b331ded14b8c2ee
sajeeshs <sajie.kl8@gmail.com>
2026-06-04 16:40:13 +0530

    day3: generated 100 bank transactions pipeline complete

--- transactions.py
+++ transactions.py
@@ -13,3 +13,31 @@ print(statuses)
 print(account_types)
 print(txn_types)
 print(countries)
+print(random.choice(branches))
+random.uniform(100,500000)
+def generate_transaction():
+    return{
+        "txn_id":"TXN001",
+        "branch":random.choice(branches),
+        "status":random.choice(statuses),
+        "account_type":random.choice(account_types),
+        "txn_type":random.choice(txn_types),
+        "country":random.choice(countries),
+        "amount":round(random.uniform(100,500000),2),
+        "customer_id":f"CUST{random.randint(1,20):03d}",
+        "txn_datetime" : datetime.now()+timedelta(days=random.randint(0,30))
+
+                      }
+transactions=[]
+for i in range(1,101):
+    txn=generate_transaction()
+    transactions.append(txn)
+
+print(generate_transaction())
+
+
+
+print(f"Total transactions: {len(transactions)}")
+
+df=pd.DataFrame(transactions)
+df.to_csv("transactions.csv",index=False)
